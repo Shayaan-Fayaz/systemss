f3b6dad8b66fb0e0a02b25b0797ba5bf2580d017
Shayaan-Fayaz <shayaanfayaz@gmail.com>
2023-12-13 18:04:22 +0530

    added profile page

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,74 +1,97 @@
-const System = require('./../models/systemModel');
+const System = require("./../models/systemModel");
+const User = require("./../models/userModel");
 
 exports.getSystemInfoPage = async (req, res, next) => {
-    const userId = req.user._id;
-    const userSystem = await System.findOne({ user: userId});
-    
-    res.status(200).render('index', {
-        title: 'System Info',
-        system: userSystem
-    });
+  const userId = req.user._id;
+  const userSystem = await System.findOne({ user: userId });
+
+  res.status(200).render("index", {
+    title: "System Info",
+    system: userSystem,
+  });
 };
 
 exports.getSignUpPage = (req, res, next) => {
-    res.status(200).render('signup', {
-        title: 'Sign Up'
-    });
+  res.status(200).render("signup", {
+    title: "Sign Up",
+  });
 };
 
 exports.getLoginPage = (req, res, next) => {
-    res.status(200).render('login', {
-        title: 'Log In'
-    })
+  res.status(200).render("login", {
+    title: "Log In",
+  });
 };
 
-
 exports.getCpuInfo = async (req, res, next) => {
-    const userId = req.user._id;
+  const userId = req.user._id;
 
-    const userSystem = await System.findOne({ user: userId});
-    res.status(200).render('cpu', {
-        title: 'CPU',
-        system: userSystem
-    })
+  const userSystem = await System.findOne({ user: userId });
+  res.status(200).render("cpu", {
+    title: "CPU",
+    system: userSystem,
+  });
 };
 
 exports.getDeviceInfo = async (req, res, next) => {
-    const userId = req.user._id;
+  const userId = req.user._id;
 
-    const userSystem = await System.findOne({ user: userId });
+  const userSystem = await System.findOne({ user: userId });
 
-    res.status(200).render('device', {
-        title: 'System Info',
-        system: userSystem
-    })
+  res.status(200).render("device", {
+    title: "System Info",
+    system: userSystem,
+  });
 };
-
 
 exports.getMemoryInfo = async (req, res, next) => {
-    // const userId = req.user._id;
+  // const userId = req.user._id;
 
-    // const userSystem = await Syste
+  // const userSystem = await Syste
 
-    res.status(200).render('memory', {
-        title: 'Memory'
-    })
+  res.status(200).render("memory", {
+    title: "Memory",
+  });
 };
 
-
 exports.getBatteryInfo = async (req, res, next) => {
-    res.status(200).render('battery', {
-        title: 'Battery'
-    })
+  res.status(200).render("battery", {
+    title: "Battery",
+  });
 };
 
 exports.getOSInfo = async (req, res, next) => {
-    const userId = req.user._id;
+  const userId = req.user._id;
 
-    const userSystem = await System.findOne({ user: userId });
+  const userSystem = await System.findOne({ user: userId });
 
-    res.status(200).render('os', {
-        title: 'OS Info',
-        system: userSystem
-    })
-}
+  res.status(200).render("os", {
+    title: "OS Info",
+    system: userSystem,
+  });
+};
+
+exports.getMyProfile = async (req, res, next) => {
+  //   const userId = req.user._id;
+  const user = req.user;
+
+  //   res.status(200).res({
+  //     status: "success",
+  //     user: user,
+  //   });
+
+  res.status(200).render("profile", {
+    title: "My Profile",
+    user: user,
+  });
+
+  //   console.log(req.user);
+
+  //   const userInfo = await User.findOne({ user: userId });
+  //   console.log(userInfo);
+
+  //   res.status(200).render("profile", {
+  //     title: "My Profile",
+  //     user: userInfo,
+  //   });
+};
--- router/viewRouter.js
+++ router/viewRouter.js
@@ -1,16 +1,27 @@
-const express = require('express');
-const authController = require('./../controllers/authController');
-const viewController = require('./../controllers/viewController');
+const express = require("express");
+const authController = require("./../controllers/authController");
+const viewController = require("./../controllers/viewController");
 
-router= express.Router();
+router = express.Router();
 
-router.route('/me').get(authController.protect, viewController.getSystemInfoPage);
-router.route('/signup').get(viewController.getSignUpPage);
-router.route('/login').get(viewController.getLoginPage);
-router.route('/cpu').get(authController.protect, viewController.getCpuInfo);
-router.route('/device').get(authController.protect, viewController.getDeviceInfo);
-router.route('/memory').get(authController.protect, viewController.getMemoryInfo);
-router.route('/battery').get(authController.protect, viewController.getBatteryInfo);
-router.route('/os').get(authController.protect, viewController.getOSInfo);
+router
+  .route("/me")
+  .get(authController.protect, viewController.getSystemInfoPage);
+router.route("/signup").get(viewController.getSignUpPage);
+router.route("/login").get(viewController.getLoginPage);
+router.route("/cpu").get(authController.protect, viewController.getCpuInfo);
+router
+  .route("/device")
+  .get(authController.protect, viewController.getDeviceInfo);
+router
+  .route("/memory")
+  .get(authController.protect, viewController.getMemoryInfo);
+router
+  .route("/battery")
+  .get(authController.protect, viewController.getBatteryInfo);
+router.route("/os").get(authController.protect, viewController.getOSInfo);
+router
+  .route("/profile")
+  .get(authController.protect, viewController.getMyProfile);
 
 module.exports = router;
